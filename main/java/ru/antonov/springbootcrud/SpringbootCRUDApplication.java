17c95348403125923ee0228a94a4dde7feaa102f
Alexander Antonov <71496401+SanyaAntonov@users.noreply.github.com>
2021-02-27 07:43:28 +0300

    Add files via upload

---
+++ main/java/ru/antonov/springbootcrud/SpringbootCRUDApplication.java
@@ -0,0 +1,13 @@
+package ru.antonov.springbootcrud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootCRUDApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringbootCRUDApplication.class, args);
+    }
+
+}
---
+++ main/java/ru/antonov/springbootcrud/repository/UserRepository.java
@@ -0,0 +1,7 @@
+package ru.antonov.springbootcrud.repository;
+
+import ru.antonov.springbootcrud.model.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface UserRepository extends JpaRepository<User, Long> {
+}
